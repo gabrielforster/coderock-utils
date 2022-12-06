e25e5ce56e8e7a05908aeecd2dd437287b9961ed
gabrielforster <rochafrgabriel@gmail.com>
2022-12-06 11:24:25 -0300

    test: write some date utils tests

---
+++ tests/date.spec.ts
@@ -0,0 +1,32 @@
+import "mocha"
+import { assert } from "chai"
+
+import date from "../src/date"
+
+describe("Date function", () => {
+	it("should be an object", () => {
+			assert.isObject(date)
+	});
+
+	it("should have a format property", () => {
+		assert.property(date, "format")
+	});
+});
+
+describe("Date Format Function", () => {
+	it("should be a function", () => {
+		assert.isFunction(date.format)
+	});
+
+	it("should return the formatted date", () => {
+		const expected = "2019-01-01"
+		const actual = date.format("2019-01-01", "yyyy-MM-dd")
+		assert.equal(actual, expected)
+	});
+
+	it("should return the formatted date", () => {
+		const expected = "13/07/2004"
+		const actual = date.format("2004-07-13", "dd/MM/yyyy")
+		assert.equal(actual, expected)
+	});
+})
