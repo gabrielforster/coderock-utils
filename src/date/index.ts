42117ee5494241d494035b36bc84037502a3487e
Gabriel Rocha <rochafrgabriel@gmail.com>
2022-12-07 20:51:12 -0300

    feat: getDaysDiff and getTimeDiff functions (#8)

--- src/date/index.ts
+++ src/date/index.ts
@@ -2,7 +2,7 @@ export function format(date: Date | string | number, format: string): string {
   const generatedDate = new Date(date);
 
 	const year = generatedDate.getUTCFullYear();
-	const month = ('0' + (generatedDate.getUTCMonth() + 1)).slice(-2)
+	const month = ('0' + (generatedDate.getUTCMonth() + 1)).slice(-2);
 	const day = ('0' + generatedDate.getUTCDate()).slice(-2);
 
 	return format.replace("yyyy", year.toString())
@@ -10,6 +10,28 @@ export function format(date: Date | string | number, format: string): string {
 	.replace("dd", day);
 };
 
+export function getDaysDiff(date: string | Date, initDate?: string | Date): number{
+	date = new Date(date);
+	initDate = initDate ? new Date(initDate) : new Date();
+
+	const diff = date.getTime() - initDate.getTime();
+
+	const days = Math.ceil(diff / (1000 * 60 * 60 * 24));
+
+	return days;
+}
+
+export function getTimeDiff(date: string | Date, initDate?: string | Date): number{
+	date = new Date(date);
+	initDate = initDate ? new Date(initDate) : new Date();
+
+	const diff = date.getTime() - initDate.getTime();
+
+	return diff;
+}
+
 export default {
-	format
+	format,
+	getDaysDiff,
+	getTimeDiff,
 };
