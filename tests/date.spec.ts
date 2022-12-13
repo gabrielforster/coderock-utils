b83c3eafbf17a0ad15c989dd6ea6541cc60fb267
Gabriel Rocha <rochafrgabriel@gmail.com>
2022-12-13 10:54:38 -0300

    tests: rewrite existing tests

--- tests/date.spec.ts
+++ tests/date.spec.ts
@@ -1,88 +1,88 @@
 import "mocha"
 import { assert } from "chai"
 
-import * as coderock from "../src"
+import * as date from "../src/date"
 
 describe("Date function", () => {
 	it("should be an object", () => {
-			assert.isObject(coderock)
+			assert.isObject(date)
 	});
 
 	it("should have a format property", () => {
-		assert.property(coderock, "format")
+		assert.property(date, "format")
 	});
 
 	it("should have a getDaysDiff property", () => {
-		assert.property(coderock, "getDaysDiff")
+		assert.property(date, "getDaysDiff")
 	})
 
 	it("should have a getTimeDiff property", () => {
-		assert.property(coderock, "getTimeDiff")
+		assert.property(date, "getTimeDiff")
 	})
 });
 
 describe("Date Format Function", () => {
 	it("should be a function", () => {
-		assert.isFunction(coderock.format)
+		assert.isFunction(date.format)
 	});
 
 	it("should return the formatted date", () => {
 		const expected = "2019-01-01"
-		const actual = coderock.format("2019-01-01", "yyyy-MM-dd")
+		const actual = date.format("2019-01-01", "yyyy-MM-dd")
 		assert.equal(actual, expected)
 	});
 
 	it("should return the formatted date", () => {
 		const expected = "13/07/2004"
-		const actual = coderock.format("2004-07-13", "dd/MM/yyyy")
+		const actual = date.format("2004-07-13", "dd/MM/yyyy")
 		assert.equal(actual, expected)
 	});
 });
 
 describe("GetDaysDiff", () => {
 	it("should be a function", () => {
-		assert.isFunction(coderock.getDaysDiff)
+		assert.isFunction(date.getDaysDiff)
 	});
 
 	it("should return the days difference with postive value", () => {
 		const expected = 1
-		const actual = coderock.getDaysDiff("2019-01-01", "2019-01-02")
+		const actual = date.getDaysDiff("2019-01-01", "2019-01-02")
 		assert.equal(actual, expected)
 	});
 
 	it("should return the days difference", () => {
 		const expected = 1
-		const actual = coderock.getDaysDiff("2019-01-02", "2019-01-01")
+		const actual = date.getDaysDiff("2019-01-02", "2019-01-01")
 		assert.equal(actual, expected)
 	});
 	
 	it("should return the days difference with negative value", () => {
 		const expected = -2
-		const actual = coderock.getDaysDiff("2022-12-06", "2022-12-08", true)
+		const actual = date.getDaysDiff("2022-12-06", "2022-12-08", true)
 		assert.equal(actual, expected)
 	})
 });
 
 describe("GetTimeDiff", () => {
 	it("should be a function", () => {
-		assert.isFunction(coderock.getTimeDiff)
+		assert.isFunction(date.getTimeDiff)
 	});
 
 	it("should return the time difference", () => {
 		const expected = -86400000
-		const actual = coderock.getTimeDiff("2019-01-01", "2019-01-02", true)
+		const actual = date.getTimeDiff("2019-01-01", "2019-01-02", true)
 		assert.equal(actual, expected)
 	});
 
 	it("should return the time difference with positive value",  () => {
 		const expected = 86400000
-		const actual = coderock.getTimeDiff("2019-01-02", "2019-01-01")
+		const actual = date.getTimeDiff("2019-01-02", "2019-01-01")
 		assert.equal(actual, expected)
 	})
 
 	it("should return the time difference", () => {
 		const expected = 0
-		const actual = coderock.getTimeDiff("2019-01-01", "2019-01-01")
+		const actual = date.getTimeDiff("2019-01-01", "2019-01-01")
 		assert.equal(actual, expected)
 	});
 });
--- tests/object.spec.ts
+++ tests/object.spec.ts
@@ -1,18 +1,28 @@
 import "mocha"
 import { assert } from "chai"
 
-import { deep } from "../src"
+import * as object from "../src/object"
+
+describe("Object tests", () => {
+	it("Should be an object", () => {
+			assert.isObject(object)
+	})
+
+	it("Should have a deep function", () => {
+		assert.isFunction(object.deep)
+	})
+})
 
 describe("Deep object test", () => {
 		it("Should be a function", () => {
-				assert.isFunction(deep)
+				assert.isFunction(object.deep)
 		})
 
 		it("Deep object pratice test", () => {
 				const expected = "banana"
 				const param = { test: {message: "banana"}}
 
-				const actual = deep(param, "test.message")
+				const actual = object.deep(param, "test.message")
 
 				assert.equal(actual, expected)
 		})
